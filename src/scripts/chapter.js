f35939c1620bb08df5423e4744c8e01a00928478
guoliyuan <guoquan_sky@163.com>
2016-06-27 01:01:58 +0800

    目录组件

--- src/scripts/chapter.js
+++ src/scripts/chapter.js
@@ -1,40 +1,40 @@
-$(document).ready(function() {
-  targetIconPos();
+// $(document).ready(function() {
+//   targetIconPos();
 
-  function targetIconPos() {
-    var chapterCur = $('.chapter-list a.active');
-    var targetIcon = $('.target-icon');
-    var targetIconPos = chapterCur.offset().top - $('.side-content').offset().top + chapterCur.height() / 2 - targetIcon.height() / 2 - $('body').scrollTop();
-    targetIcon.css('top', targetIconPos);
-  }
+//   function targetIconPos() {
+//     var chapterCur = $('.chapter-list a.active');
+//     var targetIcon = $('.target-icon');
+//     var targetIconPos = chapterCur.offset().top - $('.side-content').offset().top + chapterCur.height() / 2 - targetIcon.height() / 2 - $('body').scrollTop();
+//     targetIcon.css('top', targetIconPos);
+//   }
   
-  $('.chapter-list a').click(function(e) {
-    e.preventDefault();
-    if($(this).is('.active')) return;
-    $('.chapter-list a').removeClass('active');
-    var $target = $(this.hash);
-    var targetOffset = $target.offset().top;
-    $(this).addClass('active');
-    $('body').stop().animate({scrollTop: targetOffset}, 500);
-    targetIconPos();
-  });
+//   $('.chapter-list a').click(function(e) {
+//     e.preventDefault();
+//     if($(this).is('.active')) return;
+//     $('.chapter-list a').removeClass('active');
+//     var $target = $(this.hash);
+//     var targetOffset = $target.offset().top;
+//     $(this).addClass('active');
+//     $('body').stop().animate({scrollTop: targetOffset}, 500);
+//     targetIconPos();
+//   });
 
-  $(window).scroll(function() {
-    var targetEle = [];
-    $('.para-title').each(function(i, val) {
-      if($(window).scrollTop() >= ($(this).offset().top - 20)) {
-        var targetTit = $(this).children().attr('id');
-        targetEle.push(targetTit);
-        $('.chapter-list a').each(function() {
-          $('.chapter-list a').removeClass('active');
-          // console.log('href: ' + $(this).attr('href') + ' arr: ' + targetEle[targetEle.length - 1]);
-          if($(this).attr('href') === '#' + targetEle[targetEle.length - 1]) {
-            $(this).addClass('active');
-            targetIconPos();
-          }
-        });
-      }
-    });
-  });
+//   $(window).scroll(function() {
+//     var targetEle = [];
+//     $('.para-title').each(function(i, val) {
+//       if($(window).scrollTop() >= ($(this).offset().top - 20)) {
+//         var targetTit = $(this).children().attr('id');
+//         targetEle.push(targetTit);
+//         $('.chapter-list a').each(function() {
+//           $('.chapter-list a').removeClass('active');
+//           // console.log('href: ' + $(this).attr('href') + ' arr: ' + targetEle[targetEle.length - 1]);
+//           if($(this).attr('href') === '#' + targetEle[targetEle.length - 1]) {
+//             $(this).addClass('active');
+//             targetIconPos();
+//           }
+//         });
+//       }
+//     });
+//   });
 
-});
+// });
---
+++ src/scripts/travel.details.js
@@ -0,0 +1,118 @@
+$(document).ready(function() {
+  var menuBox = $('.menu-box');
+  var menuInner = $('.menu-inner');
+  var chapterBox = $('.chapter-box');
+  var menuStatusClass = 'fixed fixed-bottom';
+  var articleHeight = ~~($('.article').height() - $('.options-box').outerHeight());
+  var anchorPos = [];
+  var chapterItem = menuInner.find('p');
+  var chapterIcon = $('.chapter-icon');
+  var chapterIconH = chapterIcon.height();
+  var chapterIconPos = 0;
+  var customizeScrollPos = 0;
+  var curChapter = [];
+  var chapterBoxH = chapterBox.height();
+
+  menuBox.height(articleHeight);
+
+  chapterBox.mCustomScrollbar({
+    axis:"y",
+    autoHideScrollbar: true,
+    onScrollStart: function() {
+
+    }
+  });
+
+  // 存储目录锚点
+  $.each($('.J-detail-list'), function() {
+    anchorPos.push($(this).offset().top);
+  });
+  $(window).scroll(function() {
+    var sT = $('body').scrollTop();
+
+    // 固定目录
+    if(sT >= menuBox.offset().top) {
+      menuInner.removeClass(menuStatusClass).addClass('fixed');
+    } else {
+      menuInner.removeClass(menuStatusClass);
+    }
+
+    if(sT >= ($('.options-box').offset().top - menuInner.height())) {
+      menuInner.removeClass(menuStatusClass).addClass('fixed-bottom');
+    }
+
+    // 目录联动
+    $.each(anchorPos, function(i, val) {
+      if(sT + 50 >= anchorPos[i]) {
+        if($.inArray(i, curChapter) === -1) {
+          curChapter.push(i);
+        }
+      } else {
+        curChapter.splice(i, 1);
+      }
+    });
+    if(!$('body').is(':animated')) {
+      var curIndex = curChapter.length ? curChapter[curChapter.length - 1] : 0;
+      chapterItem.removeClass('active').eq(curIndex).addClass('active');
+      chapterIconPos = chapterItem.eq(curIndex).offset().top - $('.menu').offset().top + ((chapterItem.eq(curIndex).height() - chapterIconH) / 2);
+      chapterIcon.css('top', chapterIconPos);
+
+      var curChapterItem = chapterItem.filter('.active');
+      // customizeScrollPos = 0;
+      // if(curChapterItem.offset().top - chapterBox.offset().top > chapterBox.height()) {
+      //   customizeScrollPos = curChapterItem.offset().top - chapterBox.offset().top - chapterBox.height() + 40;
+      //   console.log(222);
+      // }
+      // chapterBox.mCustomScrollbar('scrollTo',customizeScrollPos);
+    }
+
+    // 目录滚动条
+    // var curChapterItem = chapterItem.filter('.active');
+    // var aa = curChapterItem.offset().top - chapterBox.offset().top - chapterBox.height() + curChapterItem.height();
+    // aa = aa < 0 ? 0 : aa;
+    // chapterBox.mCustomScrollbar('scrollTo',aa);
+  });
+
+  // 目录锚点
+  chapterItem.click(function() {
+    var _this = $(this);
+    var curIndex = chapterItem.index(_this);
+    chapterItem.removeClass('active').eq(curIndex).addClass('active');
+    $('html, body').animate({'scrollTop': anchorPos[curIndex]}, 500);
+    chapterIconPos = _this.offset().top - $('.menu').offset().top + ((_this.height() - chapterIconH) / 2);
+    chapterIcon.animate({'top': chapterIconPos}, 300);
+  });
+
+  // 禁止鼠标在目录区域，文章内容会随之滚动
+  var inner = document.getElementById('J-inner');
+  inner.onmouseenter=function(){
+    preventScroll();
+  }
+  function preventScroll(){  
+    var _this = inner;  
+    if(navigator.userAgent.indexOf("Firefox")>0){  
+      _this.addEventListener('DOMMouseScroll',function(e){  
+        _this.scrollTop += e.detail > 0 ? 60 : -60;    
+        console.log(e) 
+        e.preventDefault();  
+      },false);   
+    } else{  
+      if(_this.addEventListener){//主流浏览器  
+        _this.addEventListener('mousewheel', function(e){
+          e = e || window.event;     
+          _this.scrollTop += e.wheelDelta > 0 ? -60 : 60;    
+          e.returnValue = false; 
+          return false;  
+        }, false);  
+      } else{//IE 
+        _this.attachEvent("onmousewheel", function( e ){
+          e = e || window.event;     
+          _this.scrollTop += e.wheelDelta > 0 ? -60 : 60;    
+          e.returnValue = false; 
+          return false;  
+        });     
+      }  
+    }  
+    return this;  
+  }
+});
