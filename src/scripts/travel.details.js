124e2d24443fd1e7ea31c31cdf8bc883bd460ccf
guoliyuan <guoquan_sky@163.com>
2016-06-27 14:53:47 +0800

    添加时间转化

--- src/scripts/travel.details.js
+++ src/scripts/travel.details.js
@@ -141,4 +141,42 @@ $(document).ready(function() {
     }).scroll();  
   }
   imgLazyLoad();
+
+  // 时间格式化
+  function dateFormate(dateStr) {
+    var dateFormate = '';
+    var dateTime = new Date(dateStr);
+    var year = dateTime.getFullYear(); // 年
+    var month = dateTime.getMonth() + 1; // 月
+    var day = dateTime.getDate(); // 日
+    return dateFormate = year + '年' + month + '月' + day + '日';
+  }
+
+  // 时间转换
+  function timeago(hisTime, nowTime){
+    hisTime = new Date(hisTime).getTime();
+    var now = nowTime ? nowTime:new Date().getTime();
+    var diffValue = now - hisTime;
+    var result='';
+    var minute = 1000 * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
+
+    var _hour =diffValue/hour;
+    var _min =diffValue/minute;
+    var _day =diffValue/day;
+
+    if(_day>=1) result= dateFormate(hisTime);
+    //else if(_halfamonth>=1) result= "半个月前";
+    //else if(_week>=1) result=parseInt(_week) + "周前";
+    //else if(_day>=1) result=parseInt(_day) +"天前";
+    else if(_hour>=1) result=Math.round(_hour) +"小时前";
+    else if(_min>=1) result=Math.round(_min) +"分钟前";
+    else result="刚刚";
+    return result;
+  }
+
+  // 时间转换
+  var time = $('.timeago').attr('title');
+  $('.timeago').html(timeago(time));
 });
